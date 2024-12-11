5243bd9b5c91aaa5f989816163e116e688465d87
amWRit <derasrocks@gmail.com>
2024-12-11 19:55:00 +0545

    trying to fix cmakelists

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/My Documents/DSPProjects/AudioProcessor_Mod/include/FFTProcessor.cpp" "CMakeFiles/main.dir/include/FFTProcessor.cpp.obj" "gcc" "CMakeFiles/main.dir/include/FFTProcessor.cpp.obj.d"
+  "C:/My Documents/DSPProjects/AudioProcessor_Mod/include/GuitarIdentifier.cpp" "CMakeFiles/main.dir/include/GuitarIdentifier.cpp.obj" "gcc" "CMakeFiles/main.dir/include/GuitarIdentifier.cpp.obj.d"
+  "C:/My Documents/DSPProjects/AudioProcessor_Mod/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
